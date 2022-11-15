74b684d1e4e28f1b5cb739e3ca269738c4ae70a1
Rachit Thakker <rachit.t@solutelabs.com>
2022-11-15 11:59:26 +0530

    Used multiple fonts

--- app/next-font/page.js
+++ app/next-font/page.js
@@ -1,4 +1,10 @@
 import "../globals.css";
+import { Fjord_One, Quantico, Comic_Neue, Piazzolla } from "@next/font/google";
+
+const fjordOneFont = Fjord_One({ weight: "400", subsets: ["latin"] });
+const quanticoFont = Quantico({ weight: "400", subsets: ["latin"] });
+const comicFont = Comic_Neue({ weight: "400", subsets: ["latin"] });
+const piazzollaFont = Piazzolla({ weight: "400", subsets: ["latin"] });
 
 export default function NextFontPage() {
   return (
@@ -7,6 +13,12 @@ export default function NextFontPage() {
         <h2 className="topic-heading">
           Showcasing font optimization with @next/font
         </h2>
+        <div className="font-container">
+          <p className={`${fjordOneFont?.className}`}>Using Fjord_One</p>
+          <p className={`${quanticoFont?.className}`}>Using Quantico</p>
+          <p className={`${comicFont?.className}`}>Using Comic_Neue</p>
+          <p className={`${piazzollaFont?.className}`}>Using Piazzolla</p>
+        </div>
       </main>
     </>
   );
